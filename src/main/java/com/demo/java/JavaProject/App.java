cafc908a16a7fc0e49c2ded61b8c612814366eb1
creatorHead <droidbit1@gmail.com>
2018-04-03 15:44:31 +0530

    Added a comment in the Program

--- src/main/java/com/demo/java/JavaProject/App.java
+++ src/main/java/com/demo/java/JavaProject/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	//This will print hello world in the console
         System.out.println( "Hello World!" );
+        
     }
 }
